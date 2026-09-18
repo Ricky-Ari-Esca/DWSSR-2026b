fc4ab36ffee4c25c51e97e8543d11c085102c947
Ricky-Ari-Esca <r.ariasescarcega@gmail.com>
2026-09-18 19:39:19 +0000

    ✨ Feat: Implementar estructura básica de la aplicación Express con rutas y manejo de errores

--- server/app.js
+++ server/app.js
@@ -1,48 +1,60 @@
-//Funcion para manejar errores de la aplicacion
+// Función para manejar errores de la aplicación
 var createError = require('http-errors');
-// importa el framework express
+
+// Importa el framework Express
 var express = require('express');
-// importa modulo para manejar rutas
+
+// Importa módulo para manejar rutas
 var path = require('path');
-//importa modulo para manejar cookies 
+
+// Importa módulo para manejar cookies
 var cookieParser = require('cookie-parser');
-// importa modulo para manejar logs
+
+// Importa módulo para manejar logs
 var logger = require('morgan');
 
-//Importa las rutas de la aplicacion
+// Importa las rutas de la aplicación
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//crea la aplicacion express
+// Crea la aplicación Express
 var app = express();
 
-// configura el motor de vistas
+// Configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Configuración de middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Configura la carpeta de archivos públicos
 app.use(express.static(path.join(__dirname, 'public')));
-// configura las rutas de la aplicacion
+
+// Configura las rutas de la aplicación
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// captura de errores 404 y reenvio al manejador de errores
+// Captura de errores 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Configuración de variables locales
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Código de estado
   res.status(err.status || 500);
+
+  // Renderiza la página de error
   res.render('error');
 });
 
+// Exporta la aplicación
 module.exports = app;

24ba67b70b418f4c15ffc16bc3c60a02c381a20e
Ricky-Ari-Esca <r.ariasescarcega@gmail.com>
2026-09-25 19:37:58 +0000

    ✨ Feat: Nodemon Update users route response and enhance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,9 +3,8 @@ import express from 'express'
 const router = express.Router()
 
 /* GET users listing. */
-
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
+  res.send('<h1 style="color:red">LISTA DE AMIGAS</h1>')
 })
 
 export default router
